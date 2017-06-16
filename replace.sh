79e7d16323cc5d0602b092235ddf6b10f34f0826
wgz <wgz@pc.com>
2017-06-16 08:16:38 +0200

    Evol replace.sh

--- replace.sh
+++ replace.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
-line="      sprintf(param1, \"%d\", value);"
-regex="([\t\s]*)(sprintf)\(([a\-zA\-Z0\-9]+)(,.*)"
-regex2="\1sprintf_s\(\3, sizeof\(\3\)\4"
+#line="      sprintf(param1, \"%d\", value);"
+#regex="([\t\s]*)(sprintf)\(([a\-zA\-Z0\-9]+)(,.*)"
+#regex2="\1sprintf_s\(\3, sizeof\(\3\)\4"
 
-echo $line | sed -r "s/$regex/$regex2/g" 
-echo $line
+#echo $line | sed -r "s/$regex/$regex2/g" 
+#echo $line
+
+# when 1st param is a function call
+line="sprintf(TGV(param1, param2), \"%d\", value);"
+regex1="sprintf\(([\w\s]+)(\(.*\)), (.*)"
+regex2="sprintf_s\(\1\*2, sizeof\(\1\2)\3"
+echo $line | sed -r "s/$regex1/$regex2/g" 
